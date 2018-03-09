24e6db0684c784f5840074e4f5a94a7a6c1a48c2
ProfessorOakie <drewoken@comcast.net>
2018-03-09 01:25:15 -0800

    (Done by Zach) - golf club weapon (sucker)

---
+++ AGP-Candy-Prototype-Project/Assets/Weapons/GolfClubWeapon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfClubWeapon : Weapon {
+
+    //this is the damage scale of the "Sweet Spot"
+    [SerializeField]
+    private float mClubDamageScale;
+    private SphereCollider clubCollider;
+    // Use this for initialization
+    protected override void Start()
+    {
+        clubCollider = GetComponent<SphereCollider>();
+        base.Start();
+    }
+
+    protected override void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.CompareTag("Enemy") &&
+            (collision.contacts[0].thisCollider.Equals(clubCollider)))
+        {
+            var enemy = collision.gameObject;
+            enemy.GetComponent<EnemyHealth>().TakeDamage((collision.relativeVelocity.magnitude) * mClubDamageScale);
+            GetComponent<WeaponHealth>().TakeDamage(1);
+            if(interactableItem.AttachedHand != null)
+            {
+                rumbling = true;
+                interactableItem.AttachedHand.TriggerHapticPulse(1000, NewtonVR.NVRButtons.Touchpad);
+            }
+
+           
+        }
+        else
+        {
+            base.OnCollisionEnter(collision);
+        }
+
+    }
+
+ 
+
+
+}
---
+++ AGP-Candy-Prototype-Project/Assets/Weapons/Weapon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+    [SerializeField]
+    protected float mDamageScale;
+    protected NewtonVR.NVRInteractableItem interactableItem;
+    protected bool rumbling;
+
+    protected virtual void Start()
+    {
+        interactableItem = GetComponent<NewtonVR.NVRInteractableItem>();
+    }
+
+    //protected virtual void Update()
+    //{
+    //    if (rumbling && interactableItem.AttachedHand != null)
+    //    {
+    //        interactableItem.AttachedHand.TriggerHapticPulse(500);
+    //    }
+    //}
+
+
+    protected virtual void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            var enemy = collision.gameObject;
+            enemy.GetComponent<EnemyHealth>().TakeDamage((collision.relativeVelocity.magnitude)*mDamageScale);
+            Debug.LogWarning("Hardcoded Weapon Damage");
+            GetComponent<WeaponHealth>().TakeDamage(1);
+            if (interactableItem.AttachedHand != null)
+            {
+                rumbling = true;
+
+                interactableItem.AttachedHand.TriggerHapticPulse(1000, NewtonVR.NVRButtons.Touchpad);
+                
+            }
+
+        }
+
+
+    }
+
+    
+}
