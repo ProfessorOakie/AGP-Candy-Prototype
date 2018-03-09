2c5b2ba89263fb48cca7ce40b48645c9e87a2638
Drew Okenfuss <okenfuss@usc.edu>
2018-03-09 01:18:16 -0800

    - enemies spawn in waves endlessly - enemies spawn from SpawnZones

---
+++ AGP-Candy-Prototype-Project/Assets/Scripts/Spawning/SpawnZone.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnZone : MonoBehaviour {
+
+    private BoxCollider boxSpawnZone;
+
+    [SerializeField]
+    private int spawnPriority = 1;
+
+    [SerializeField]
+    private GameObject[] SpawnablePrefabs;
+
+    private void Start()
+    {
+        boxSpawnZone = GetComponent<BoxCollider>();
+        boxSpawnZone.isTrigger = true;
+    }
+
+    // Spawns a random prefab from the list in a random spot in the box
+    public GameObject SpawnObject()
+    {
+        if (SpawnablePrefabs.Length > 0)
+        {
+            int indexToSpawn = Random.Range(0, SpawnablePrefabs.Length);
+            GameObject spawnedObject = Instantiate(SpawnablePrefabs[indexToSpawn], GetSpawnPosition(), GetSpawnRotation());
+            return spawnedObject;
+        }
+        else
+        {
+            Debug.LogError("Tried to spawn an object on a SpawnZone with no SpawnablePrefabs attached.");
+            return null;
+        }
+    }
+
+    private Vector3 GetSpawnPosition()
+    {
+        return transform.position + RandomPointInBox(boxSpawnZone.center, boxSpawnZone.size);
+    }
+
+    private Vector3 RandomPointInBox(Vector3 center, Vector3 size)
+    {
+        return center + new Vector3(
+           (Random.value - 0.5f) * size.x,
+           (Random.value - 0.5f) * size.y,
+           (Random.value - 0.5f) * size.z
+        );
+    }
+
+    private Quaternion GetSpawnRotation()
+    {
+        Debug.LogWarning("TODO: figure out what designer wants this to be");
+        return transform.rotation;
+    }
+
+    public int GetSpawnPriority() { return spawnPriority; }
+
+}
---
+++ AGP-Candy-Prototype-Project/Assets/Scripts/Spawning/WaveManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : Singleton<WaveManager> {
+
+    private int WaveNumber = 0;
+    private int SpawnZonePriorityTotals = 0;
+
+    // A list of all the spawn zones that exist
+    private SpawnZone[] SpawnZones;
+
+    private void Start()
+    {
+        SpawnZones = FindObjectsOfType<SpawnZone>();
+        foreach (var zone in SpawnZones)
+            SpawnZonePriorityTotals += zone.GetSpawnPriority();
+
+        NewWave();
+    }
+    
+    // The last enemy was just killed
+    public void EndOfWave()
+    {
+        StartCoroutine(WaitForNextWave());
+    }
+
+    // Time inbetween waves
+    IEnumerator WaitForNextWave()
+    {
+        Debug.LogWarning("TODO: Implemment hardcoded value");
+        yield return new WaitForSeconds(3);
+        NewWave();
+    }
+
+    // Spawns new enemies in the zones based on their priority.
+    private void NewWave()
+    {
+        ++WaveNumber;
+        Debug.LogWarning("TODO: make this number calculated as designer specifies");
+        int numEnemiesToBeSpawned = WaveNumber * 3; 
+        foreach(var zone in SpawnZones)
+        {
+            int numEnemiesToSpawnInZone = Mathf.FloorToInt(((float)zone.GetSpawnPriority() / (float)SpawnZonePriorityTotals * (float) numEnemiesToBeSpawned) + 0.5f);
+            for (int i = 0; i < numEnemiesToSpawnInZone; ++i)
+            {
+                // Check enemy was successfully spawned
+                if (zone.SpawnObject())
+                    GameManager.Instance.NumLivingEnemiesIncrement();
+            }
+        }
+    }
+    
+
+}
