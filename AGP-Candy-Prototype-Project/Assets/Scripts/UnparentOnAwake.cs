7ed046c54d7c5d544dd5e9344964dd127487dd54
ProfessorOakie <drewoken@comcast.net>
2018-03-12 20:12:30 -0700

    Skinner changed to work with Burger

---
+++ AGP-Candy-Prototype-Project/Assets/Scripts/UnparentOnAwake.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnparentOnAwake : MonoBehaviour
+{
+    // Happens before Start (I think)
+    private void Awake()
+    {
+        // Appends the name of the previous parent to the end of the name.
+        gameObject.name = gameObject.name + " (" + transform.parent.name + ")";
+
+        // Does not alter the transform values
+        transform.SetParent(null, false);
+    }
+
+}
