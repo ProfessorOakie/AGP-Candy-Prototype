66ee9e65a76a21ba193b043936a5c4cb2c5aa84e
ProfessorOakie <drewoken@comcast.net>
2018-03-21 00:47:46 -0700

    Shredder added for deleting enemies outside the map

---
+++ AGP-Candy-Prototype-Project/Assets/Scripts/Shredder.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour {
+    
+    // Destroys anything that exits
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.GetComponent<Health>())
+            other.gameObject.GetComponent<Health>().Die();
+        Destroy(other.gameObject);
+    }
+
+}
