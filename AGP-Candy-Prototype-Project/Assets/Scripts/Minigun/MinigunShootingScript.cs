5ba5777b47644e9e102887ccc22120e57f825a2d
DESKTOP-GLT4Q2I\sakus <kushell@usc.edu>
2018-03-28 16:01:44 -0700

    rotating shooting working

---
+++ AGP-Candy-Prototype-Project/Assets/Scripts/Minigun/MinigunShootingScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class MinigunShootingScript : MonoBehaviour {
+
+    // Static constants
+    static int NUM_BARRELS = 8;
+
+    // Shot tracking
+    [SerializeField]
+    private GameObject rayCastSource;
+    [SerializeField]
+    private GameObject[] bulletSources;
+    private int currBulletSource;
+
+    // Wait time
+    [SerializeField]
+    [Range(0,5f)]
+    [Tooltip("How many seconds between shots when holding down trigger")]
+    private float timeBetweenShots;
+    private float coolDownTimer = 0;
+
+    //Bullet prefab
+    [SerializeField]
+    private GameObject skittleBulletPrefab;
+
+
+    private void Start()
+    {
+        currBulletSource = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsTriggerPulled())
+        {
+            if (coolDownTimer <= 0)
+            {
+                Fire();
+                LoadNextBarrel();
+                coolDownTimer = timeBetweenShots;
+            }
+        }
+        if (coolDownTimer > 0)
+        {
+            coolDownTimer -= Time.fixedDeltaTime;
+        }
+    }
+
+    private bool IsTriggerPulled()
+    {
+        return true;
+    }
+
+    private void Fire()
+    {
+        // Get initial position
+        Vector3 _sourcePos = bulletSources[currBulletSource].transform.position;
+        // Spawn in bullet
+        GameObject _skittleBullet = (GameObject)Instantiate(skittleBulletPrefab, _sourcePos, Quaternion.identity);
+        SkittleBulletScript _skittleBulletInfo = _skittleBullet.GetComponent<SkittleBulletScript>();
+        // Raycast to find target
+        Vector3 _target = _sourcePos + Vector3.forward * 100f; //temp
+        // Pass target to skittlebullet
+        _skittleBulletInfo.target = _target;
+        Debug.Log("Setting _skittleBulletInfo.target to" + _skittleBulletInfo.target.ToString());
+    }
+
+    private void LoadNextBarrel()
+    {
+        currBulletSource = (currBulletSource + 1) % NUM_BARRELS;
+    }
+}
+
+//PUT BULLET SPEED IN HERE!
---
+++ AGP-Candy-Prototype-Project/Assets/Scripts/Minigun/SkittleBulletScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class SkittleBulletScript : MonoBehaviour {
+
+    // Array of colors of classic Skittles as found online
+    private static Color[] colors =
+    {
+        new Color(230f/255f,72f/255f,8f/255f),    // Orange
+    	new Color(241f/255f,190f/255f,2f/255f),   // Yellow
+    	new Color(4f/255f,130f/255f,7f/255f),     // Green
+    	new Color(68f/255f,19f/255f,73f/255f),    // Purple
+    	new Color(192f/255f,4f/255f,63f/255f)     // Red
+    };
+
+    // Bullet Speed
+    [SerializeField]
+    [Range(0,5f)]
+    private float bulletSpeed;
+    private static float staticBulletSpeed;
+
+    // Destination
+    [HideInInspector]
+    public Vector3 target = Vector3.zero;
+
+    // Direction
+    private bool isDirectionFound = false;
+    private Vector3 direction = Vector3.zero;
+
+
+    // Rigidbody
+    private Rigidbody rb;
+
+
+    void Start () {
+        // Initialize values
+        staticBulletSpeed = bulletSpeed;
+        rb = GetComponent<Rigidbody>();
+        // Set to random skittle color on start
+        Renderer _rend = GetComponentInChildren<Renderer>();
+        _rend.material.color = colors[Random.Range(0, colors.Length)];
+
+        Debug.Log("Starting skittlebulletscripr!");
+    }
+
+    private void FixedUpdate()
+    {
+        // Set direction if not found already, but only if target is acquired
+        if (!isDirectionFound)
+        {
+            Debug.Log("SkittleBulletScript Fixed Update: target = " + target.ToString());
+            if (target != Vector3.zero)
+            {
+                direction = (target - transform.position).normalized;
+                isDirectionFound = true;
+            }
+        }
+        // Move along direction otherwise
+        else
+        {
+            rb.MovePosition(transform.position + direction * (staticBulletSpeed * Time.fixedDeltaTime));
+        }
+    }
+
+
+}
