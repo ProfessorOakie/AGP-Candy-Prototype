dfc3451671c2421ff7f7188a9d348914d4987567
alexander-lee <thealexanderlee@me.com>
2018-03-29 10:16:09 -0700

    title menu integrated

---
+++ AGP-Candy-Prototype-Project/Assets/Scripts/Hovering.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hovering : MonoBehaviour {
+
+	[SerializeField]
+	private float speed;
+
+	[SerializeField]
+	private float period;
+
+	[SerializeField]
+	private float verticalShift;
+
+	[SerializeField]
+	private float horizontalShift;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Transform transform = GetComponent<Transform>();
+		Vector3 newPos = transform.position;
+		newPos.y = period * Mathf.Cos((speed * Time.time) + horizontalShift) + verticalShift;
+
+		transform.position = newPos;
+	}
+}
